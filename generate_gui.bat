60a0e603d818e74ebd35498365821fcdfea6411c
Rustam Second_Fry Gubaydullin <secondfry@gmail.com>
2019-04-16 09:33:06 +0300

    Automatically use relative imports in generated UI

--- generate_gui.bat
+++ generate_gui.bat
@@ -1,4 +1,4 @@
-pyside2-uic resources\ui\gui_main.ui -o src\shortcircuit\view\gui_main.py
-pyside2-uic resources\ui\gui_tripwire.ui -o src\shortcircuit\view\gui_tripwire.py
-pyside2-uic resources\ui\gui_about.ui -o src\shortcircuit\view\gui_about.py
+pyside2-uic --from-imports resources\ui\gui_main.ui -o src\shortcircuit\view\gui_main.py
+pyside2-uic --from-imports resources\ui\gui_tripwire.ui -o src\shortcircuit\view\gui_tripwire.py
+pyside2-uic --from-imports resources\ui\gui_about.ui -o src\shortcircuit\view\gui_about.py
 pyside2-rcc resources\resources.qrc -o src\shortcircuit\view\resources_rc.py
