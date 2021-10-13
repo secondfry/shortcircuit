59a0a7b3c822a2241ee36b17f98c758eb8053917
Rustam SecondFry Gubaydullin <secondfry@gmail.com>
2021-10-13 21:47:40 +0300

    Version bump (v0.5.1)

--- src/shortcircuit/__init__.py
+++ src/shortcircuit/__init__.py
@@ -6,6 +6,6 @@
 
 
 __appname__ = 'Short Circuit'
-__version__ = '0.5.1-rc.1'
-__date__ = '2021-10-02'
+__version__ = '0.5.1'
+__date__ = '2021-10-13'
 USER_AGENT = '{} v{} ({})'.format(__appname__, __version__, __date__)
