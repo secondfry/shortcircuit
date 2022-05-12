a50b632e1006a719a130028ddeb3a8d76612760b
Rustam Gubaydullin <secondfry@yandex-team.ru>
2022-05-12 21:57:58 +0300

    Fix icon path in build_win_installer.bat

--- build_win_installer.bat
+++ build_win_installer.bat
@@ -2,6 +2,6 @@ python -O -m PyInstaller ^
     --clean ^
     --onefile ^
     --windowed ^
-    --icon resources\images\app_icon.ico ^
+    --icon src\resources\images\app_icon.ico ^
     --name shortcircuit src\main.py ^
     --paths "build\libs" ^
