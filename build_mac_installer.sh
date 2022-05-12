c0867d6ae59cab523b3a5db29d8f77c27f125673
Rustam Gubaydullin <secondfry@yandex-team.ru>
2022-05-12 23:11:46 +0300

    Commit icns app icon for CI

--- build_mac_installer.sh
+++ build_mac_installer.sh
@@ -4,5 +4,5 @@ python -O -m PyInstaller \
     --noupx \
     --clean \
     --windowed \
-    --icon src/shortcircuit/resources/images/app_icon.ico \
+    --icon src/shortcircuit/resources/images/app_icon.icns \
     --name shortcircuit src/main.py
