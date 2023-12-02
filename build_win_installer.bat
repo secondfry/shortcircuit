747dba4e064b95008f02303afb9b7b62ce3ef4b9
Rustam SecondFry Gubaydullin <secondfry+github@gmail.com>
2023-12-03 01:52:06 +0300

    Fix --add-data flag of PyInstaller

--- build_win_installer.bat
+++ build_win_installer.bat
@@ -2,7 +2,7 @@ python -O -m PyInstaller ^
     --clean ^
     --windowed ^
     --icon resources\images\app_icon.ico ^
-    --add-data "src\database;." ^
+    --add-data "src\database\*;database" ^
     --noconfirm ^
     --name shortcircuit src\main.py ^
     --onefile ^
