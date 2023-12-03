164ec4ab71931cee6cc92ac6b34e0d41d1cf830e
Rustam @SecondFry Gubaydullin <secondfry+github@gmail.com>
2023-12-03 06:37:06 +0300

    Fix build_mac_installer.sh, replace colon with semicolon

--- build_mac_installer.sh
+++ build_mac_installer.sh
@@ -4,7 +4,7 @@ python -O -m PyInstaller \
     --clean \
     --windowed \
     --icon resources/images/app_icon.icns \
-    --add-data 'src/database/*:database' \
+    --add-data 'src/database/*;database' \
     --noconfirm \
     --name shortcircuit src/main.py \
     --noupx \
